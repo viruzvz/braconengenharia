2a39383a57655f06630155c2f936cbe4f1588793
cegn <cegn@senado.gov.br>
2016-03-03 15:56:13 -0300

    atualizacao html

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
 ;!function () {
   $(document).ready(function() {
   $('.thumbnail').click(function(){
-        $('.modal-body').empty();
+        $('.modal-body--js').empty();
       var title = $(this).parent('a').attr("title");
-      $('.modal-title').html(title);
-      $($(this).parents('div').html()).appendTo('.modal-body');
+      $('.modal-title--js').html(title);
+      $($(this).parents('div').html()).appendTo('.modal-body--js');
       $('#galeria1').modal({show:true});
   });
   });
